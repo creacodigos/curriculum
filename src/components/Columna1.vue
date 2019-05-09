f40a3a2d835d873453748e69b002ea1b68dbaf38
iMachito <jorgesantanarodriguez@gmail.com>
2019-05-09 12:11:42 +0100

    VUE: Estructura de componentes. Creadas Columnas y simplificado Main

---
+++ src/components/Columna1.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="one">
+    <section aria-labelledby="datos">
+      <article>
+        <img src="/assets/img/jorge_santana.jpg" alt="Foto Jorge Santana">
+      </article>
+      <article>
+        <div>
+          <ul>
+            <li><strong><i class="fas fa-envelope"></i>EMAIL:</strong> <a href="mailto:jorgesantanarodriguez@gmail.com">jorgesantanarodriguez@gmail.com</a></li>
+            <li><strong><i class="fas fa-baby-carriage"></i>HIJO:</strong> <a href="http://muscana.com" target="_blank" title="Portal de Músicos Canarios">Muscana.com</a></li>
+            <li><strong><i class="fas fa-robot"></i>WEB:</strong> <a href="http://creacodigos.com">creacodigos.com</a></li>
+            <li><strong><i class="fab fa-linkedin"></i>LINKEDIN:</strong> <a href="https://www.linkedin.com/in/jorgesantanarodriguez/" target="_blank">jorgesantanarodriguez</a></li>
+          </ul>
+        </div>
+      </article>
+    </section>
+    <section aria-labelledby="habilidades">
+      <article>
+        <h4 class="titulo">
+          <i class="fas fa-fire"></i>habilidades
+        </h4>
+        <div>
+          <ul>
+            <li><strong>PHP 5:</strong> <meter min="0" max="100" value="80"></meter></li>
+            <li><strong>MYSQL:</strong> <meter min="0" max="100" value="75"></meter></li>
+            <li><strong>HTML, CSS:</strong> <meter min="0" max="100" value="90"></meter></li>
+            <li><strong>HTML5, CSS3:</strong> <meter min="0" max="100" value="75"></meter></li>
+            <li><strong>JAVASCRIPT:</strong> <meter min="0" max="100" value="60"></meter></li>
+            <li><strong>GIT:</strong> <meter min="0" max="100" value="40"></meter></li>
+
+            <li><strong>VUE:</strong> <meter min="0" max="100" value="40"></meter></li>
+            <li><strong>JQUERY:</strong> <meter min="0" max="100" value="50"></meter></li>
+            <li><strong>NPM:</strong> <meter min="0" max="100" value="30"></meter></li>
+            <li><strong>Apache:</strong> <meter min="0" max="100" value="60"></meter></li>
+            <li><strong>GESTIÓN DNS:</strong> <meter min="0" max="100" value="90"></meter></li>
+          </ul>
+        </div>
+      </article>
+    </section>
+  </div>    
+</template>
+
+<script>
+export default {
+	name: 'Columna1'
+}
+</script>
---
+++ src/components/Columna3.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="three">
+    <section aria-labelledby="Formación Académica">
+      <article>
+        <h4 class="titulo">
+          <i class="fas fa-user-graduate"></i>Formación Académica
+        </h4>
+        <div>
+          <ul>
+            <li>
+              <h5>Técnico Superior Desarrollo de Aplicaciones Web</h5>
+              <h5 class="cargo">
+                C.I.F.P. César Manrique
+              </h5>
+              <p>
+                Desarrollar, implantar, y mantener aplicaciones web, con independencia del modelo empleado y utilizando tecnologías específicas, garantizando el acceso a los datos de forma segura y cumpliendo los criterios de accesibilidad, usabilidad y calidad exigidas en los estándares establecidos. JAVA SE, PHP, XNL, CSS, FTP, DNS.
+              </p>
+              <h6 class="cargo">
+                2015 - 2018
+              </h6>
+            </li>
+            <li>
+              <h5>Licenciado en Bellas Artes - Diseño</h5>
+              <h5 class="cargo">
+                U.L.L.
+              </h5>
+              <p>
+                Cursando el itinerario de Diseño Gráfico. 
+              </p>
+              <h6 class="cargo">
+                2000 - 2005
+              </h6>
+            </li>
+          </ul>
+        </div>
+      </article>
+    </section>
+
+    <section aria-labelledby="Cursos no reglados">
+      <article>
+        <h4 class="titulo">
+          <i class="fas fa-chalkboard-teacher"></i>Cursos no reglados
+        </h4>
+        <div>
+          <ul>
+            <li>
+              <strong>Introducción a Vue.JS</strong>
+              <p>
+                Fundación General de la Universidad de La Laguna
+              </p>                                
+            </li>
+            <li>
+              <strong>Crear un API RESTful con Slim PHP y usarla con AJAX</strong>
+              <p>Udemy</p>
+            </li>
+            <li>
+              <strong>Programación con JavaScript</strong>
+              <p>Fundación General de la Universidad de La Laguna</p>
+            </li>
+            <li>
+              <strong>Aprende a programar aplicaciones Móviles</strong>
+              <p>Miriadax</p>
+            </li>
+
+                            
+            <li>
+              <strong>Máster JAVA SE8</strong>
+              <p>Intecssa 2018 - 2019</p>
+            </li>
+          </ul>
+        </div>
+      </article>
+    </section>
+ 
+    <section aria-labelledby="aficiones">
+      <article>
+        <h4 class="titulo">
+          <i class="fas fa-guitar"></i>Aficiones
+        </h4>
+        <div>
+          <p>
+            La Música, La guitarra, el bajo, la batería, el cine, las series, la tecnología, los cachivaches, las ofertas de pccomponentes y el chocolate.
+          </p>
+        </div>
+      </article>
+    </section>             
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Columna3'
+}
+</script>
