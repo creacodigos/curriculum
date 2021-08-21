1e6a53b20ea5c3ad2e3e61d8e0e2637f01e84475
Jorge Santana <jorge@creacodigos.com>
2021-08-21 13:47:30 +0100

    Añadiendo redes y scroll to top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,21 @@
 <template>
   <footer>
     <div>
-      {{ datos.datos.nombre }} - @creacodigos 
+      Desde 🏝 Tenerife para el mundo 🚀
+      <div class="redes">
+        <a href="https://github.com/creacodigos" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+        <a :href=`https://www.linkedin.com/in/${datos.datos.linkedin}/` target="_blank">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="https://twitter.com/creacodigos" target="_blank">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </div> 
+      <div class="top" @click="scrollToTop()">
+        <i class="fas fa-arrow-alt-circle-up"></i>
+      </div>
     </div>
   </footer>    
 </template>
@@ -11,6 +25,36 @@ export default {
 	name: 'CrFooter',
   data() {
       return this.$parent
+  },
+  methods:{
+    scrollToTop(){
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
   }
 }
 </script>
+
+<style scoped>
+footer{
+  text-transform:none;
+  font-size: 1rem;
+}
+
+.top{
+  position: fixed;
+  bottom: 2rem;
+  right: 1rem;
+}
+.top i{
+  font-size: 3rem;
+  color: #43cbff;
+  cursor: pointer;
+}
+.redes i{
+  font-size: 2rem;
+  margin: 1rem;
+}
+.redes a i{
+  color: white;
+}
+</style>
